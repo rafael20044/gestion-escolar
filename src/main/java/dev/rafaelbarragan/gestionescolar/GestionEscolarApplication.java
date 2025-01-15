62ba79a33a63d5070c6737a47d351c964727b958
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-01-14 23:27:19 -0500

    first commit

---
+++ src/main/java/dev/rafaelbarragan/gestionescolar/GestionEscolarApplication.java
@@ -0,0 +1,13 @@
+package dev.rafaelbarragan.gestionescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rafaelbarragan/gestionescolar/GestionEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelbarragan.gestionescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
